830ac71fa267efbef190cd505773d38c57498dc0
bhanu <bhanu@LAPTOP-DQRV4G9K.lan>
2020-08-23 14:48:26 -0400

    Added a class ImperativeVsDeclarativeEx1

---
+++ Java8-Udemy/src/com/java/mycourses/ImperativeVsDeclarativeEx1.java
@@ -0,0 +1,30 @@
+package com.java.mycourses;
+
+import java.util.stream.IntStream;
+
+public class ImperativeVsDeclarativeEx1 {
+
+	public static void main(String[] args) {
+		/**
+		 * Imperative - How Style of Programming
+		 */
+		
+		int imperativeSum = 0;
+		
+		for (int i = 0; i <= 100; i++) {
+			imperativeSum += i;
+		}
+		
+		System.out.println("Sum using Imperative approach: " + imperativeSum);
+		
+		/**
+		 * Declarative - What Style of Programming
+		 */
+		
+		int declarativeSum = IntStream.rangeClosed(0, 100)
+				.parallel()
+				.sum();
+		System.out.println("Sum using declarative approach: " + declarativeSum);
+	}
+
+}
